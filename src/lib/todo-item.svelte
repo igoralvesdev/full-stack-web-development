2beb360d52625d5a8deba1c490c931c522e680fa
Igor Alves <igoralves.dev@proton.me>
2022-07-17 04:36:25 +0000

    Implement HTML

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,15 @@
+<div>
+    <form action="" method="">
+        <input type="hidden" name="done" value="">
+        <button arial-label="mark done/not done">Done/Donen't</button>
+    </form>
+
+    <form action="">
+        <input type="text"/>
+        <button name="save todo">Save</button>
+    </form>
+
+    <form action="">
+        <button aria-label="delete todo">Delete</button>
+    </form>
+</div>
